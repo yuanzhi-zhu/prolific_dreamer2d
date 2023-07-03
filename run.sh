a0c717a3998b4a7cacedd685a04e86449765647c
YuanzhiZhu <yuazhu@student.ethz.ch>
2023-07-03 22:49:50 +0200

    add colab notebook

--- run.sh
+++ run.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
 
-### Image generation with prolific_dream 2d 
+# Image generation with prolific_dream 2d 
+### VSD
 python prolific_dreamer2d.py \
-        --num_steps 500 --t_end 1000 --log_steps 50 --dtype float \
-        --lr 0.03 --phi_lr 0.0001 --use_t_phi true \
-        --use_scheduler false --lr_scheduler_start_factor 0.333 --lr_scheduler_iters 200 \
+        --num_steps 500 --log_steps 72 \
+        --seed 1024 --lr 0.03 --phi_lr 0.0001 --use_t_phi true \
         --model_path 'stabilityai/stable-diffusion-2-1-base' \
-        --loss_weight '1m_alphas_cumprod' \
-        --t_schedule random --generation_mode 'vsd' \
-        --phi_model 'lora' --lora_scale 1. --lora_vprediction true\
+        --loss_weight_type '1m_alphas_cumprod' --t_schedule 'random' \
+        --generation_mode 'vsd' \
+        --phi_model 'lora' --lora_scale 1. --lora_vprediction false \
         --prompt "a photograph of an astronaut riding a horse" \
         --height 512 --width 512 --batch_size 1 --guidance_scale 7.5 \
-        --log_progress true --save_x0 true --save_phi_model true --multisteps 1 \
-        # --load_phi_model_path 'work_dir/20230615_2254_vsd_cfg_7.5_bs_2_lr_0.03_philr_0.0001_lora'
+        --log_progress true --save_x0 true --save_phi_model true \
+
+# ### VSD multi particles
+# python prolific_dreamer2d.py \
+#         --num_steps 1500 --log_steps 50 \
+#         --seed 1024 --lr 0.03 --phi_lr 0.0001 --use_t_phi true \
+#         --model_path 'stabilityai/stable-diffusion-2-1-base' \
+#         --loss_weight_type '1m_alphas_cumprod' --t_schedule 'random' \
+#         --generation_mode 'vsd' \
+#         --phi_model 'lora' --lora_scale 1. --lora_vprediction false \
+#         --prompt "a photograph of an astronaut riding a horse" \
+#         --height 512 --width 512 --batch_size 16 --guidance_scale 7.5 \
+#         --particle_num_vsd 2 --particle_num_phi 2 \
+#         --log_progress false --save_x0 false --save_phi_model true
+
+# ### SDS
+# python prolific_dreamer2d.py \
+#         --num_steps 500 --log_steps 50 \
+#         --lr 0.03 --phi_lr 0.0001 --use_t_phi true \
+#         --model_path 'runwayml/stable-diffusion-v1-5' \
+#         --loss_weight '1m_alphas_cumprod' \
+#         --t_schedule random --generation_mode 'sds' \
+#         --prompt "a photograph of an astronaut riding a horse" \
+#         --height 512 --width 512 --batch_size 1 --guidance_scale 7.5 \
+#         --log_progress true --save_x0 true \
